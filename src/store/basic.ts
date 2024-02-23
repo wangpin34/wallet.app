b999dcbc80ad598c6145f2a0deae73c5b241a1b6
pin-wang <pin.wang@hp.com>
2024-02-23 11:27:45 +0800

    feat: add provider store

--- src/store/basic.ts
+++ src/store/basic.ts
@@ -1,5 +1,6 @@
 import { defineStore } from 'pinia'
 import { QingWallet } from 'utils/wallet'
+import { Provider, ethers } from 'ethers'
 
 export const useBasicStore = defineStore('basic', {
   state: () => ({
@@ -10,6 +11,24 @@ export const useBasicStore = defineStore('basic', {
       this.isLocked = isLocked
     },
   },
+})
+
+interface ProviderEntity {
+  provider: Provider
+  label: string
+  isTestNetwork?: boolean
+}
+
+const defaultProvider: ProviderEntity = {
+  provider: new ethers.JsonRpcProvider('http://127.0.0.1:7545'),
+  label: '7545',
+  isTestNetwork: true
+}
+
+export const useProviderStore = defineStore('provider', {
+  state: () => ({
+    provider: defaultProvider
+  })
 })
 
 export const useWalletStore = defineStore('wallet', {
