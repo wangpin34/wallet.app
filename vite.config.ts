d80c55be0c8def3a720ae8aee2a4d670cc8d6317
penn wang <guyusay@gmail.com>
2024-02-25 13:32:47 +0800

    cicd: fix buid issue

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,14 @@ import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag) => ['qr-code'].includes(tag),
-        },
-      },
-    }),
+    vue(),
     UnoCSS(),
     viteTsconfigPaths(),
+    nodePolyfills()
   ],
 })
